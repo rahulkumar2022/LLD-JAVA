29729010a5ae7c2f344e83e571f84a106acc4132
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-01-04 23:37:56 +0530

    inside and outside pannel implementations

--- Elevator-System LLD/InsidePannel.java
+++ Elevator-System LLD/InsidePannel.java
@@ -1,4 +1,34 @@
 package Elevator_System_LLD;
 
-public class InsidePannel {
+import Elevator_System_LLD.enums.DoorAction;
+import Elevator_System_LLD.enums.FloorNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsidePannel implements Pannel {
+
+    private List<ElevatorButton> elevatorButtonList;
+    private List<DoorButton> doorButtons;
+
+    public InsidePannel(){
+        elevatorButtonList = new ArrayList<>();
+        doorButtons = new ArrayList<>();
+
+        for(int i=0;i<15;i++){
+            elevatorButtonList.add(new ElevatorButton(false, FloorNumber.values()[i]));
+        }
+
+        for(int i=0;i<3;i++){
+            doorButtons.add(new DoorButton(false, DoorAction.values()[i]));
+        }
+    }
+
+    public boolean pressFloorButton(int floorNumber){
+        return elevatorButtonList.get(floorNumber).press();
+    }
+    public boolean pressDoorButton(int doorNumber){
+        return doorButtons.get(doorNumber).press();
+    }
+
 }
--- Elevator-System LLD/OutSidePannel.java
+++ Elevator-System LLD/OutSidePannel.java
@@ -1,4 +1,25 @@
 package Elevator_System_LLD;
 
-public class OutSidePannel {
+import Elevator_System_LLD.enums.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutSidePannel implements Pannel{
+    private List<HallButton> hallButtons;
+
+    public OutSidePannel(){
+        hallButtons = new ArrayList<>();
+        hallButtons.add(new HallButton(false, Direction.UP));
+        hallButtons.add(new HallButton(false,Direction.DOWN));
+        hallButtons.add(new HallButton(false,Direction.IDLE));
+    }
+
+    public void moveUp(){
+        System.out.println("Moving upward called");
+    }
+
+    public void moveDown(){
+        System.out.println("Moving in downward direction called");
+    }
 }
