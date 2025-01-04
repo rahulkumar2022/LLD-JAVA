6377a9cda72a1fffca368224517825e291afbcb6
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-01-04 23:09:08 +0530

    hallbutton elevator lld

---
+++ Elevator-System LLD/HallButton.java
@@ -0,0 +1,41 @@
+package Elevator_System_LLD;
+
+import Elevator_System_LLD.enums.Direction;
+
+public class HallButton implements Button{
+
+    private boolean status;
+    private Direction direction;
+
+    public HallButton(boolean status,Direction direction){
+        this.status = status;
+        this.direction = direction;
+    }
+
+    public HallButton(){
+
+    }
+
+    public Direction getDirection(){
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean isPressed() {
+        return status;
+    }
+
+    @Override
+    public boolean press() {
+        status = !status;
+        return status;
+    }
+}
